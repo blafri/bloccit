063fd8def514b044e03229084529d30cb68585e7
Blayne Farinha <blayne.farinha@gmail.com>
2015-03-04 15:57:12 +0000

    association module complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,27 @@
 
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 # Create Posts
 50.times do
     Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-  )
+      user:  users.sample,
+      title: Faker::Lorem.sentence,
+      body:  Faker::Lorem.paragraph
+    )
 end
 
 posts = Post.all
@@ -21,12 +36,21 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'blayne.farinha@gmail.com',
+  password: 'C00lness',
+  name: 'Blayne Farinha'
+)
+
 puts "Seed finished"
+puts "#{User.count} users in database."
 puts "#{Post.count} posts in database."
 puts "#{Comment.count} comments in database."
-
