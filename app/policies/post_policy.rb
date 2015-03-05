7132417f693a18f0b440cdd23df6720215c4378f
Blayne Farinha <blayne.farinha@gmail.com>
2015-03-05 12:18:06 +0000

    Complete Authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,22 @@ class PostPolicy < ApplicationPolicy
     true
   end
   
+  class Scope < Scope
+    
+    def resolve
+      # check if a user is signed in
+      # if not signed in return an empty Post::ActiveRecord_Relation
+      return scope.none unless user
+      
+      # If user is an admin or moderator they can see everything
+      # Else they can only see there posts
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        user.posts
+      end
+    end
+    
+  end
+  
 end
