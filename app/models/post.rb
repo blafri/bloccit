2a0987696209275d6c124a3db7fbcdb488e19fdd
Blayne Farinha <blayne.farinha@gmail.com>
2015-03-13 18:30:53 +0000

    Complete uploading images assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   
+  mount_uploader :image, PostUploader
+  
   default_scope { order('created_at DESC') }
   
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+  validates :image, presence: true
 end
