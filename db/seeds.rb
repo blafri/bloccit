721cb9a63fca294094c2fe0a0cb15053ceb8d935
Blayne Farinha <blayne.farinha@gmail.com>
2015-03-19 18:30:39 +0000

    comments module complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ topics = Topic.all
 users = User.all
 
 # Create Posts
-500.times do
+50.times do
     Post.create!(
       user:  users.sample,
       topic: topics.sample,
@@ -46,7 +46,7 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
-    # user: users.sample,   # we have not yet associated Users with Comments
+    user: users.sample,
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
