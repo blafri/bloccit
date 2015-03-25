31d89de9ba3fe08f9a7ca5a1237ec37c2ba152fc
Blayne Farinha <blayne.farinha@gmail.com>
2015-03-25 19:09:25 +0000

    Mocking with RSPEC module complete

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 describe Vote, type: :model do
+  include TestFactories
+  
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         expect(Vote.new(value: 5).valid?).to eq(false)
       end
+    end
+  end
+  
+  describe 'after_save' do
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 end
