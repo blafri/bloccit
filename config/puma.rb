9a235ee2efd5edbabe40fc77b64c532552919d7a
Blayne Farinha <blayne.farinha@gmail.com>
2015-04-07 13:19:03 +0000

    Changed puma config file default max threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,10 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads_count = Integer(ENV['MAX_THREADS'] || 1)
 threads threads_count, threads_count
 
 preload_app!
+
+
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
