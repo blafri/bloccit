3927b44201a50442df2dca6fda4e925f5925febb
Blayne Farinha <blayne.farinha@gmail.com>
2015-04-10 00:35:12 +0000

    Removed unneeded comments from user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,7 @@ require 'rails_helper'
 describe User do
   
   before do
-    # Create a user and a post
-    #@user1 = create(:user)
-    #@post1 = create(:post, user: @user1)
-    #create(:comment, user: @user1, post: @post1)
     @user1 = create(:user_with_post_and_comment)
-    
-    # Create a second user and a post
-    #@user2 = create(:user)
-    #@post2 = create(:post, user: @user2)
-    #2.times { create(:comment, user: @user2, post: @post2) }
     @user2 = create(:user_with_post_and_comment, comment_count: 2)
   end
   
