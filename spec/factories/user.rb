c0eeee3ae240f649bfeadd39e3ccc11c75593433
Blayne Farinha <blayne.farinha@gmail.com>
2015-04-10 00:26:41 +0000

    Complete testing users assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,17 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+    
+    factory :user_with_post_and_comment do
+      transient do
+        post_count 1
+        comment_count 1
+      end
+      
+      after(:create) do |user, evaluator|
+        create_list(:post, evaluator.post_count, user: user)
+        create_list(:comment, evaluator.comment_count, user: user)
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,31 +4,33 @@ describe User do
   
   before do
     # Create a user and a post
-    @user1 = create(:user)
-    @post1 = create(:post, user: @user1)
-    create(:comment, user: @user1, post: @post1)
+    #@user1 = create(:user)
+    #@post1 = create(:post, user: @user1)
+    #create(:comment, user: @user1, post: @post1)
+    @user1 = create(:user_with_post_and_comment)
     
     # Create a second user and a post
-    @user2 = create(:user)
-    @post2 = create(:post, user: @user2)
-    2.times { create(:comment, user: @user2, post: @post2) }
+    #@user2 = create(:user)
+    #@post2 = create(:post, user: @user2)
+    #2.times { create(:comment, user: @user2, post: @post2) }
+    @user2 = create(:user_with_post_and_comment, comment_count: 2)
   end
   
   describe "#favorited(post)" do
     it "returns `nil` if the user has not favorited the post" do
-      expect( @user1.favorited(@post2) ).to eq(nil)
+      expect( @user1.favorited(@user2.posts.first) ).to eq(nil)
     end
     
     it "returns the appropriate favortie if it exists" do
-      favorite = create(:favorite, user: @user1, post: @post2)
-      expect( @user1.favorited(@post2) ).to eq(favorite)
+      favorite = create(:favorite, user: @user1, post: @user2.posts.first)
+      expect( @user1.favorited(@user2.posts.first) ).to eq(favorite)
     end
     
     it "returns `nil` if the user has favorited another post" do
       new_post = create(:post)
       create(:favorite, user: @user1, post: new_post)
       
-      expect( @user1.favorited(@post2) ).to eq(nil)
+      expect( @user1.favorited(@user2.posts.first) ).to eq(nil)
     end
   end
   
