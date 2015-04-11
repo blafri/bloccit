802790081c450110879703031101d973b35821a7
Blayne Farinha <blayne.farinha@gmail.com>
2015-04-11 15:06:11 +0000

    Complete destroying Ajax checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,16 +18,18 @@ class CommentsController < ApplicationController
   
   def destroy
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
     @comment = @post.comments.find(params[:id])
     
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+    end
+    
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
   
